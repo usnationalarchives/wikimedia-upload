039ecd6c6ad4153f326c9ea0852c8ad041ea2586
Dominic McDevitt-Parks <mcdevitd@gmail.com>
2017-06-15 10:45:07 -0400

    ignore settings file for now

--- settings.py
+++
@@ -1,9 +0,0 @@
-# This a sample settings file. This setup allows changes to configurations—what collection of files to upload, user credentials, and categories—to be made in this simpler file, without any changes needing to be made in the main script.
-
-user = u'XXX'
-password = 'XXX'
-
-api_query = u'https://catalog.archives.gov/api/v1?q=cats&type=description&exists=objects&cursorMark='
-
-categories = """[[Category:Cats]]
-[[Category:Archival photographs]]"""
